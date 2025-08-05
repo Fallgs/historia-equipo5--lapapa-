d60a2990d055abfc197bc033553754cdf4dcf5c2
cherryali <citlalivm222@gmail.com>
2025-08-04 20:10:00 -0600

    Agrega parte 3 de la historia

--- historia.py
+++ historia.py
@@ -2,3 +2,5 @@ print("Parte 1: Juan salió de su casa muy temprano.")
 # Parte 2
 print("Parte 2: Se encontró con un dragón en la entrada del bosque.")
 # Parte 3
+print("Parte 3: Al final, el dragón resultó ser su mascota perdida.")  
+# Integrante 3
